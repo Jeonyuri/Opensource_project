9e671f6d07adaf4c96622283bddb6b3126b93add
Jeonyuri <127704512+Jeonyuri@users.noreply.github.com>
2023-10-13 14:28:13 +0900

    Update 계산기 코드.py

--- src/계산기 코드.py
+++ src/계산기 코드.py
@@ -9,17 +9,14 @@ def add(x, y):
 ​
  def div(x, y):
     return x / y
-​
  print("사칙연산을 선택 하세요:")
  print("1.더하기")
  print("2.빼기")
  print("3.곱하기")
  print("4.나누기")
  user = input("선택 하세요(1,2,3,4):")
-​
  n1 = int(input("첫번째 숫자 : "))
  n2 = int(input("두번째 숫자 : "))
-​
  if user == '1':
     print(num1,"+",num2,"=", add(n1,n2))
 ​
